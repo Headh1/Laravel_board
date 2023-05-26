de0ccdfce6240281ee4376bd5ffdde6fe944e563
Headh1 <a.design804@gmail.com>
2023-05-26 17:46:35 +0900

    2023.05.26

---
+++ board/app/Http/Controllers/BoardsController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\boards;
+use Illuminate\Support\Facades\DB;
+
+class BoardsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $result = Boards::select(['id','title','hits','created_at','updated_at'])->orderBy('hits','desc')->get();
+        return view('list')->with('data',$result);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('write');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $req)
+    {
+        $boards = new Boards([
+            'title' => $req->input('title')
+            ,'content' => $req->input('content')
+        ]);
+        $boards->save();
+        return redirect('/boards');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $boards = Boards::find($id);
+        $boards->hits++;
+        $boards->save();
+        return view('detail')->with('data', Boards::findOrFail($id));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $boards = Boards::find($id);
+        
+        return view('edit')->with('data', $boards);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+
+        // $boards =  
+        DB::table('boards')->where('id',$id)->update([
+            'title' => request('title'),
+            'content' => request('content'),
+        ]);
+
+        $result = Boards::find($id);
+        $result->title = $request->title;
+        $result->content = $request->content;
+        $result->save();
+
+        
+        return redirect('/boards/'.$id)->with('data', Boards::findOrFail($id));
+        // return view('detail')->with('data', Boards::findOrFail($id));
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        // $boards = Boards::find($id);
+        $boards = DB::update('update boards set deleted_at = NOW() where id = :id', ['id'=>$id] );
+        // $boards->save();
+        // return redirect('/boards');
+        // $boards->delete();
+        return redirect('/boards');
+    }
+}
---
+++ board/resources/views/detail.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+    글번호 : {{$data->id}}
+    <br>
+    제목 : {{$data->title}}
+    <br>
+    내용 : {{$data->content}}
+    <br>
+    등록일: {{$data->created_at}}
+    <br>
+    수정일 : {{$data->updated_at}}
+    <br>
+    조회수 : {{$data->hits}}
+    <br>
+    </div>
+    <button type="button" onclick="location.href='{{route('boards.index')}}'">list</button>
+    <button type="button" onclick="location.href='{{route('boards.edit',['board' => $data->id])}}'">update</button>
+    
+    <form action="{{route('boards.destroy',['board' => $data->id])}}" method="post">
+        @csrf
+        @method('DELETE')
+        <button type="submit" onclick="alert('삭제?')">delete</button>
+    </form>
+</body>
+</html>
