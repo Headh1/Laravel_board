e2e345b8abe4e8274a3132f99493d641f7c2f07e
Headh1 <a.design804@gmail.com>
2023-06-02 17:52:19 +0900

    2023.06.02

--- board/app/Http/Controllers/UserController.php
+++ board/app/Http/Controllers/UserController.php
@@ -14,10 +14,22 @@ use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class UserController extends Controller
 {
     function login() {
+        $arr['key'] = 'test';
+        $arr['kim'] = 'park';
+        Log::emergency('emergency',$arr);
+        Log::alert('alert',$arr);
+        Log::critical('critical',$arr);
+        Log::error('error',$arr);
+        Log::warning('warning',$arr);
+        Log::notice('notice',$arr);
+        Log::info('info',$arr);
+        Log::debug('debug',$arr);
+
         return view('login');
     }
 
